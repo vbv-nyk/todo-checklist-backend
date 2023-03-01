667bd36bd05022f231a585d8d9e97028270afa5f
Vaibhav Nayak <vbv.nyk.2003@gmail.com>
2023-03-01 18:18:47 +0530

    Add cors

--- app.js
+++ app.js
@@ -1,11 +1,13 @@
 const { urlencoded, json } = require("express");
 const express = require("express");
 const mongoose = require("mongoose");
+const cors = require("cors");
 const todosRouter = require("./routes/Todos");
 
 const app = express();
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
+app.use(cors());
 
 mongoose.connect("mongodb+srv://pixel:abcd1234@cluster0.ykxlaxs.mongodb.net/?retryWrites=true&w=majority");
 

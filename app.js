cc9b82b282a64423020ba04cb4bb6a1efb204abe
Vaibhav Nayak <vbv.nyk.2003@gmail.com>
2023-03-01 16:20:47 +0530

    Add urlencoded and express.json()

--- app.js
+++ app.js
@@ -1,8 +1,12 @@
+const { urlencoded, json } = require("express");
 const express = require("express");
 const mongoose = require("mongoose");
 const todosRouter = require("./routes/Todos");
 
 const app = express();
+app.use(express.urlencoded({ extended: false }));
+app.use(express.json());
+
 mongoose.connect("mongodb+srv://pixel:abcd1234@cluster0.ykxlaxs.mongodb.net/?retryWrites=true&w=majority");
 
 app.use("/Todos", todosRouter);
